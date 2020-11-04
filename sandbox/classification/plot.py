3e91e8977b72df8b08fb6e1bb6c59c3aca7532d4
Antoine Petit <pe.antoine@hotmail.be>
2020-11-04 14:32:49 +0100

    plot on all csv (tjrs plus)

--- sandbox/classification/plot.py
+++ sandbox/classification/plot.py
@@ -1,32 +1,37 @@
 import matplotlib.pyplot as plt
-import csv
-import os
-import numpy as np
+import csv,os
 
-path_delay_csv = "../data/csv/delay2020-11-02.csv"
+path = "../data/csv/"
 
-f= open(path_delay_csv,'rt')
-with f:
-	data= csv.reader(f)
-	big_late=0
-	late=0
-	nul=0
-	avance= 0
-	grosse_avance=0
-	for row in data:
-		if len(row[3]) >0:
-			
-			delay= float(row[3]) 			
-			if delay>10:				
-				grosse_avance+=1
-			elif delay>0:
-				avance +=1
-			elif delay==0:
-				nul +=1
-			elif delay> -10:
-				late+=1
-			else:
-				big_late +=1
+for root, dirs, files in os.walk("../data/csv"):
+	pass
+
+big_late=0
+late=0
+nul=0
+avance= 0
+grosse_avance=0
+
+for file in files:
+	path = "../data/csv/"
+	path+=file
+	f= open(path,'rt')
+	with f:
+		data= csv.reader(f)
+
+		for row in data:
+			if len(row[3]) >0:
+				delay= float(row[3]) 			
+				if delay>10:				
+					grosse_avance+=1
+				elif delay>0:
+					avance +=1
+				elif delay==0:
+					nul +=1
+				elif delay> -10:
+					late+=1
+				else:
+					big_late +=1
 
 x= ['big late', 'late', 'on time', 'early','big early']
 delay=[big_late,late,nul,avance,grosse_avance]
