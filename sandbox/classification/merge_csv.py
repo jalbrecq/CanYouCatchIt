0b5a0101e0bd9b7147e581f7be0323ba04987b72
Antoine Petit <pe.antoine@hotmail.be>
2020-11-08 15:44:26 +0100

    the merge change the delay in five type

--- sandbox/classification/merge_csv.py
+++ sandbox/classification/merge_csv.py
@@ -14,4 +14,14 @@ merged_df = pd.concat(all_df, ignore_index=True, sort=False)
 
 merged_df=merged_df.drop(merged_df[merged_df.delay.isna()].index)
 
+merged_df.loc[merged_df.delay >=10.0,"delay"]= 10
+
+merged_df.loc[(merged_df.delay >0) & (merged_df.delay <10.0),"delay"]= 1
+
+merged_df.loc[merged_df.delay ==0,"delay"]= 0
+
+merged_df.loc[(merged_df.delay >-10) & (merged_df.delay <0),"delay"]= -1
+
+merged_df.loc[merged_df.delay <=-10,"delay"]= -10
+
 merged_df.to_csv('../data/csv/merged.csv',index=False)
