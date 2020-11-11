e84e8ef710529df3b5207dddaa407f16065f4b4f
Antoine Petit <pe.antoine@hotmail.be>
2020-11-11 12:22:20 +0100

    loop on random forest

--- sandbox/classification/randomforests.py
+++ sandbox/classification/randomforests.py
@@ -10,9 +10,11 @@ data = pd.read_csv(path,1,",")
 X=data[['direction', 'date_month', 'date_weekday', 'date_hour','date_minute','date_seconde','temp','humidity','visibility','wind','rain']]  # Features
 y=data['delay']  # Labels
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5,random_state=10) # 70% training and 30% test
 
-clf=RandomForestClassifier(n_estimators=100,criterion="entropy")
-clf.fit(X_train,y_train)
-y_pred=clf.predict(X_test)
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+for i in range(5):
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5) # 70% training and 30% test
+
+	clf=RandomForestClassifier(n_estimators=100,criterion="entropy",bootstrap=True,min_samples_leaf=300)
+	clf.fit(X_train,y_train)
+	y_pred=clf.predict(X_test)
+	print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
