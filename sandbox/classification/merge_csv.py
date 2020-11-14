40f85a1e5b9f871d2121b4e721b1aaeebcfbbeb2
Antoine Petit <pe.antoine@hotmail.be>
2020-11-14 15:14:37 +0100

    merge into 3 and 5 classes

--- sandbox/classification/merge_csv.py
+++ sandbox/classification/merge_csv.py
@@ -15,13 +15,24 @@ merged_df = pd.concat(all_df, ignore_index=True, sort=False)
 merged_df=merged_df.drop(merged_df[merged_df.delay.isna()].index)
 
 merged_df.loc[merged_df.delay >=10.0,"delay"]= 10
-
 merged_df.loc[(merged_df.delay >0) & (merged_df.delay <10.0),"delay"]= 1
-
 merged_df.loc[merged_df.delay ==0,"delay"]= 0
-
 merged_df.loc[(merged_df.delay >-10) & (merged_df.delay <0),"delay"]= -1
-
 merged_df.loc[merged_df.delay <=-10,"delay"]= -10
 
 merged_df.to_csv('../data/csv/merged.csv',index=False)
+
+
+merged_3_df = pd.concat(all_df, ignore_index=True, sort=False)
+
+merged_3_df=merged_3_df.drop(merged_3_df[merged_3_df.delay.isna()].index)
+
+
+merged_3_df.loc[merged_3_df.delay >0,"delay"]= 1
+merged_3_df.loc[merged_3_df.delay ==0,"delay"]= 0
+merged_3_df.loc[merged_3_df.delay <0,"delay"]= -1
+
+
+merged_3_df.to_csv('../data/csv/merged_3.csv',index=False)
+
+
